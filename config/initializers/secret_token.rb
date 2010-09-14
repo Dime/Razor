d0103d44f441af099db25bab322110ece5b0bc83
Dime <dime@pashoski.com>
2010-09-15 01:46:07 +0200

    razor rails3 startup with boilerplate

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Razor::Application.config.secret_token = 'fc6f16137ab382ad3d55a8c13b092cee0e5bfeb388ac4fb95e67acf9f97052d58f05853496306e7a90138496d8ade157bf0b574148739664696d47f3bf27a60f'
